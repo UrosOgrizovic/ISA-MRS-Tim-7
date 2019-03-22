9f3c63d0071e485d221ce908b0b504e125eaba18
pax <ivkovicdjordje1997@gmail.com>
2019-03-22 14:11:36 +0100

    Added initial project, JPA and security dependence commented in pom.xml

---
+++ FlightsReservations/src/main/java/com/flightreservation/FlightsReservations/FlightsReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.flightreservation.FlightsReservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsReservationsApplication.class, args);
+	}
+
+}
---
+++ FlightsReservations/src/test/java/com/flightreservation/FlightsReservations/FlightsReservationsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flightreservation.FlightsReservations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightsReservationsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
