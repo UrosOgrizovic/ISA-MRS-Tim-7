f942106012149ddf892c124400d375e2a2060329
UrosOgrizovic <uros.ogrizovic671@gmail.com>
2019-03-27 17:19:25 +0100

    Car search fix

--- FlightsReservations/src/main/resources/static/js/searchCars.js
+++ FlightsReservations/src/main/resources/static/js/searchCars.js
@@ -1,24 +1,42 @@
 var searchUrl = "http://localhost:8080/racss/searchCars";
+var mapa;
+var emailSelect;
 
-function searchCars() {
-    if (!validateInputs()) {
-		alert("Inputs are invalid!");
-		return;
-	}
-    var name = $("#name").val();
-    var yearOfManufacture = $("#yearOfManufacture").val();
-    var manufacturer = $("#manufacturer").val();
+$(document).ready(function(){
+    $("#carSearchForm").on('submit', function(e) {
+        e.preventDefault();
+        if (!validateInputs()) {
+            alert("Inputs are invalid!");
+            return;
+        }
+        var name = $("#name").val();
+        var yearOfManufacture = $("#yearOfManufacture").val();
+        var manufacturer = $("#manufacturer").val();
+    
+        $.ajax({
+            url: searchUrl + "?name=" + name + "&manufacturer=" + manufacturer + "&yearOfManufacture=" + yearOfManufacture,
+            method: "GET",
+            dataType: "json",
+            contentType: "application/json",
+            success: function(result) {
+                $("#foundCars").remove();
+                displaySearchResults(result);
+            }, error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+    
+    
+});
 
-    $.ajax({
-		url: searchUrl + "?name=" + name + "&manufacturer=" + manufacturer + "&yearOfManufacture=" + yearOfManufacture,
-		method: "GET",
-		contentType: "application/json",
-		dataType: "json",	
-		data: {},
-		success: function(result) {
-            console.log(result);
-		}
-	});
+function displaySearchResults(cars) {
+    var text = "<div id=\"foundCars\"><h2>Search results: <h2><br>";
+    for (car of cars) {
+        text += "<h3>"+ " " + car.manufacturer + " " + car.name + " " + car.yearOfManufacture + "</h3><br>";
+    }
+    text += "</div>";
+    $(document.documentElement).append(text);
 }
 
 function validateInputs() {
@@ -31,4 +49,12 @@ function validateInputs() {
 			break;
 		}
 	return flag;
+}
+
+function setInputs(){
+	var key = emailSelect.val();
+	$("#firstName").val(mapa[key].firstName);
+	$("#lastName").val(mapa[key].lastName);
+	$("#phone").val(mapa[key].phone);
+	$("#address").val(mapa[key].address);
 }
