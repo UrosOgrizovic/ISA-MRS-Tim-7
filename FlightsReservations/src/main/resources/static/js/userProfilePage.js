70f0c862fb4889cef4087637f1250a16cbcaf97d
Uros Ogrizovic <uros.ogrizovic671@gmail.com>
2019-04-27 12:04:05 +0200

    Add view all car reservations frontend & cancel car reservations frontend

--- FlightsReservations/src/main/resources/static/js/userProfilePage.js
+++ FlightsReservations/src/main/resources/static/js/userProfilePage.js
@@ -2,53 +2,61 @@ var getAllFriendsLink = "http://localhost:8080/users/getFriends";
 var getAllCarReservationsLink = "http://localhost:8080/users/getCarReservations";
 var getAllFlightReservationsLink = "http://localhost:8080/users/getFlightReservations";
 var getAllHotelReservationsLink = "http://localhost:8080/users/getHotelReservations";
+var cancelCarReservationLink = "http://localhost:8080/carReservations/cancel/";
 
 
 $(document).ready(function(){
     $("#viewAllFriends").on('click', function(e) {
-        var email = $("#email").text();
         e.preventDefault();
-        
-        $("#all").remove();
-        $("#error").remove();
-        
-        $.ajax({
-            url: getAllFriendsLink,
-            method: "POST",
-            dataType: "json",
-            contentType: "application/json",
-            data: JSON.stringify(email),
-            success: function(friends) {
-                displayFriends(friends);
-            }, error: function(error) {
-                $(document.documentElement).append("<h3 id=\"error\">Error</h3>");
-                console.log(error);
-            }
-        });
+        getAllFriends();
     });
 
-    $("#viewAllReservations").on('click', function(e) {
-        var email = $("#email").text();
+    $("#viewAllCarReservations").on('click', function(e) {
         e.preventDefault();
-        $("#all").remove();
-        $("#error").remove();
-        
-        $.ajax({
-            url: getAllReservationsLink,
-            method: "POST",
-            dataType: "json",
-            contentType: "application/json",
-            data: JSON.stringify(email),
-            success: function(carReservations) {
-                displayCarReservations(carReservations);
-            }, error: function(error) {
-                $(document.documentElement).append("<h3 id=\"error\">Error</h3>");
-                console.log(error);
-            }
-        });
-
+        getAllCarReservations();
     })
 });
+
+function getAllCarReservations() {
+    var email = $("#email").text();
+    $("#all").remove();
+    $("#error").remove();
+    
+    $.ajax({
+        url: getAllCarReservationsLink,
+        method: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(email),
+        success: function(carReservations) {
+            displayCarReservations(carReservations);
+        }, error: function(error) {
+            $(document.documentElement).append("<h3 id=\"error\">Error</h3>");
+            console.log(error);
+        }
+    });
+}
+
+function getAllFriends() {
+    var email = $("#email").text();
+
+    $("#all").remove();
+    $("#error").remove();
+    
+    $.ajax({
+        url: getAllFriendsLink,
+        method: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(email),
+        success: function(friends) {
+            displayFriends(friends);
+        }, error: function(error) {
+            $(document.documentElement).append("<h3 id=\"error\">Error</h3>");
+            console.log(error);
+        }
+    });
+}
 
 function displayFriends(friends) {
     var text = "<table id=\"all\" style= \"margin:20px; width: 90%; float: center; text-align: center;\" class=\"table table-striped\">";
@@ -73,11 +81,58 @@ function displayFriends(friends) {
         
         text += "</tr>";
     }
+    
     text += "</tbody></table>";
     $(document.documentElement).append(text);
     
 }
 
 function displayCarReservations(carReservations) {
+    var text = "<table id=\"all\" style= \"margin:20px; width: 90%; float: center; text-align: center;\" class=\"table table-striped\">";
+    text += "<caption>All car reservations</caption>"
+    text += "<thead>";
+    text += "<tr>";
+    text += "<th>Price (USD)</th>";
+    text += "<th>Confirmed</th>";
+    text += "<th>Date of reservation</th>";
+    text += "<th>Start time</th>";
+    text += "<th>End time</th>";
+    text += "</tr>";
+    text += "</thead><tbody>";
+    for (var cr of carReservations) {
+        text += "<tr>";
 
+        text += "<td>" + cr.price + "</td>";
+        if (cr.confirmed) {
+            
+            text += "<td>" + "<button class=\"btn btn-secondary\" onClick=\"cancelCarReservation("+cr.id+")\">Cancel</button>" + "</td>";
+        } else {
+            text += "<td>Canceled</td>";
+        }
+        
+        text += "<td>" + cr.dateOfReservation + "</td>";
+        text += "<td>" + cr.startTime + "</td>";
+        text += "<td>" + cr.endTime + "</td>";
+        
+        text += "</tr>";
+    }
+    
+    text += "</tbody></table>";
+    $(document.documentElement).append(text);
+}
+
+function cancelCarReservation(id) {
+    $.ajax({
+        url: cancelCarReservationLink + id,
+        method: "PUT",
+        dataType: "json",
+        contentType: "application/json",
+        data: {},
+        success: function() {
+            getAllCarReservations();
+        }, error: function(error) {
+            $(document.documentElement).append("<h3 id=\"error\">Error</h3>");
+            console.log(error);
+        }
+    });
 }
