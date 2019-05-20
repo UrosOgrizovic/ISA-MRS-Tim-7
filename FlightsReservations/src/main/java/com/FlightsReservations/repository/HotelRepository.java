1cfa88c26c88a3ad8dfaf8e12c0ae41a2f016b3b
MilosBujakovic <milosbujakovic97@gmail.com>
2019-05-20 21:36:16 +0200

    Backend work in progress

--- FlightsReservations/src/main/java/com/FlightsReservations/repository/HotelRepository.java
+++ FlightsReservations/src/main/java/com/FlightsReservations/repository/HotelRepository.java
@@ -12,6 +12,7 @@ public interface HotelRepository extends JpaRepository<Hotel, Long>{
 	
 	//@Query(value = "SELECT * FROM hotels WHERE hotel_name=")
 	Hotel findByName(String name);
+	List<Hotel> findByLatitudeAndLongitude(Float latitude, Float longitude);
 	
 	@Query(
 		value = "SELECT * FROM hotels WHERE hotel_id=?1 AND hotel_id=?2",
@@ -22,6 +23,4 @@ public interface HotelRepository extends JpaRepository<Hotel, Long>{
 	
 	List<Hotel> findAll();
 	
-	
-	Hotel save(Hotel hotel);
 }
