314afefe50e0d8c9fd2caa62ef7215e6d89225cb
Uros Ogrizovic <uros.ogrizovic671@gmail.com>
2019-06-17 23:22:23 +0200

    Add optimistic and pesimistic locks

--- FlightsReservations/src/main/java/com/FlightsReservations/repository/CarRepository.java
+++ FlightsReservations/src/main/java/com/FlightsReservations/repository/CarRepository.java
@@ -1,11 +1,19 @@
 package com.FlightsReservations.repository;
 
 
+import java.util.Optional;
+
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import com.FlightsReservations.domain.Car;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
+	// reading not allowed (so that no one else can use the same car to make a car reservation), writing not allowed
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	Optional<Car> findById(Long id);
 }
